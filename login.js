8fa107aefaf4439aff96c7bda80ff124d7d63592
FullStackAce <hellosammy007@gmail.com>
2025-01-09 17:07:46 -0600

    changed pw

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
 // login.js
-const correctPassword = "FSA"; // Replace with your actual password
+const correctPassword = "imaginebeingaddicted"; // Replace with your actual password
 
 // Check if user is already logged in
 if (sessionStorage.getItem("isLoggedIn")) {
@@ -17,12 +17,5 @@ document
       // Password is correct, set login status and redirect to index.html
       sessionStorage.setItem("isLoggedIn", "true"); // Set login status
       window.location.href = "index.html"; // Redirect to index.html after login
-
-      // Start the inactivity timer
-      startInactivityTimer();
-    } else {
-      // Show error message if password is incorrect
-      document.getElementById("errorMessage").innerText =
-        "Incorrect password. Please try again.";
     }
   });
